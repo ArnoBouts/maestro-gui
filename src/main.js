badd2f05bb8655f56611f5d8acbe4cc998373b13
Arnaud Bouts <arnaud.bouts@softeam.fr>
2017-08-30 09:39:41 +0200

    switch from bulma to buefy

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
+import Buefy from 'buefy'
 import 'font-awesome/css/font-awesome.css'
-import 'bulma/css/bulma.css'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
+Vue.use(Buefy, {
+  defaultIconPack: 'fa'
+})
 
 /* eslint-disable no-new */
 new Vue({
